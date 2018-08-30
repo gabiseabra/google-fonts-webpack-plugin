e800091c77bc9cdc2cf6fb81bcd85128b733d1dd
Erik E. Seierstad <es@dib.no>
2018-08-30 18:05:38 +0200

    updated to work with Webpack 4

--- spec/GoogleWebfontsPlugin.js
+++ spec/GoogleWebfontsPlugin.js
@@ -1,7 +1,7 @@
-const should = require("should")
-const webpack = require("webpack")
-const MemoryFs = require("memory-fs")
-const GoogleWebfontPlugin = require("../src")
+const should = require("should");
+const webpack = require("webpack");
+const MemoryFs = require("memory-fs");
+const GoogleWebfontPlugin = require("../src");
 
 const webpackConfig = {
 	entry: require.resolve("../package.json"),
@@ -12,13 +12,14 @@ const webpackConfig = {
 	stats: {
 		colors: true
 	}
-}
+};
 
 describe("GoogleWebfontPlugin", () => {
 	let fs, plugin, stats
 
-	before(() => {
-		fs = new MemoryFs()
+	//before
+	it("does something", () => {
+		fs = new MemoryFs();
 		plugin = new GoogleWebfontPlugin({
 			filename: "styles/fonts.css",
 			fonts: [
@@ -30,14 +31,15 @@ describe("GoogleWebfontPlugin", () => {
 					family: "Roboto"
 				}
 			]
-		})
+		});
 		const compiler = webpack(Object.assign({}, webpackConfig, {
 			plugins: [ plugin ]
-		}))
+		}));
 		compiler.outputFileSystem = fs
 		return new Promise((resolve, reject) => {
 			compiler.run((err, data) => {
 				if(err) {
+					console.log(err);
 					reject(err)
 				} else {
 					stats = data
@@ -46,22 +48,20 @@ describe("GoogleWebfontPlugin", () => {
 					resolve(stats)
 				}
 			})
-		})
-	})
+		});
+	});
 
 	it("adds a chunk to compilation", () => {
-		stats.compilation.namedChunks
-		.should.have.key(plugin.options.name)
-	})
+		Array.from(stats.compilation.namedChunks.keys()).should.containEql(plugin.options.name);
+	});
 
 	it("emits font files", () => {
-		fs.existsSync("/font/SourceSansPro-Regular.woff").should.be.ok()
-		fs.existsSync("/font/Roboto-Regular.woff").should.be.ok()
-	})
+		fs.existsSync("/font/SourceSansPro-Regular.woff").should.be.ok();
+		fs.existsSync("/font/Roboto-Regular.woff").should.be.ok();
+	});
 
 	it("emits a css file", () => {
-		fs.existsSync("/styles/fonts.css").should.be.ok()
-		fs.readFileSync("/styles/fonts.css", "utf8")
-		.should.containEql("../font/Roboto-Regular.woff")
-	})
-})
+		fs.existsSync("/styles/fonts.css").should.be.ok();
+		fs.readFileSync("/styles/fonts.css", "utf8").should.containEql("../font/Roboto-Regular.woff")
+	});
+});
--- src/index.js
+++ src/index.js
@@ -1,129 +1,170 @@
 const _ = require("lodash")
 const path = require("path")
 const Chunk = require("webpack/lib/Chunk")
-const { RawSource } = require("webpack-sources")
+const {
+    RawSource
+} = require("webpack-sources")
 const GoogleWebfonts = require("./GoogleWebfonts")
 const cssUrl = require("./cssUrl")
 
 const defaults = {
-	fonts: undefined,
-	name: "fonts",
-	apiUrl: undefined,
-	formats: undefined,
-	filename: "fonts.css",
-	path: "font/",
-	local: true
+    fonts: undefined,
+    name: "fonts",
+    apiUrl: undefined,
+    formats: undefined,
+    filename: "fonts.css",
+    path: "font/",
+    local: true
 }
 
+const pluginSignature = {
+	name: "GoogleWebfontsPlugin"
+};
+
 class GoogleWebfontsPlugin {
-	constructor(options) {
-		this.options = Object.assign({}, defaults, options)
-		this.chunk = new Chunk(this.options.name)
-		this.chunk.ids = []
-		this.chunk.name = this.options.name
-	}
+    constructor (options) {
+        this.options = Object.assign({}, defaults, options)
+        this.chunk = new Chunk(this.options.name)
+        this.chunk.ids = []
+        this.chunk.name = this.options.name
+    }
 
-	get api() {
-		if(!this._api) {
-			this._api = new GoogleWebfonts(this.options.apiUrl)
-		}
-		return this._api
-	}
-
-	fetch() {
-		const {
-			fonts,
-			apiUrl,
-			path: fontsPath,
-			filename: cssFile,
-			formats: defaultFormats
-		} = this.options
-		const compareCss = (a, b) => (a.id.localeCompare(b.id))
-		const fontsCss = []
-		const files = {}
-		const promises = []
-		fonts.forEach((fontOptions) => {
-			const { family } = fontOptions
-			const query = this.api.getFontByFamily(family)
-				.then(font => {
-					if(!font) {
-						throw new Error(`Font family \"${family}\" not found.`)
-					}
-					return font.select(_.assign(
-						fontOptions,
-						{ formats: defaultFormats }
-					))
-				})
-			const cssRelativePath = path.posix.relative(
-				path.dirname(cssFile),
-				fontsPath
-			)
-			promises.push(
-				query.then(selection => {
-					return selection.css(cssRelativePath).then((css) => {
-						fontsCss.push({
-							css,
-							id: selection.font.id
-						})
-					})
-				})
-			)
-			if(fontsPath) {
-				promises.push(
-					query.then(q => q.assets())
-					.then(assets => {
-						for(const fileName in assets) {
-							files[fontsPath + fileName] = assets[fileName]
-						}
-					})
-				)
-			}
-		})
-		return Promise.all(promises)
-			.then(() => {
-				fontsCss.sort(compareCss)
-				const css = fontsCss.map(font => font.css).join("\n")
-				return { css, files }
-			})
-	}
-
-	apply(compiler) {
-		const { fonts, local, filename: cssFile } = this.options
-		compiler.plugin("make", (compilation, cb) => {
-			if(local) {
-				const addFile = (fileName, source) => {
-					this.chunk.files.push(fileName)
-					compilation.assets[fileName] = source
-				}
-				this.fetch().then(({ css, files }) => {
-					addFile(cssFile, new RawSource(css))
-					for(const fileName in files) {
-						addFile(fileName, files[fileName])
-					}
-					cb()
-				})
-			} else {
-				cb()
-			}
-			compilation.plugin("html-webpack-plugin-before-html-generation", (data, cb) => {
-				if (local && (data.assets.publicPath.indexOf("://") !== -1 || data.assets.publicPath.indexOf(":") !== -1)) {
-          data.assets.css.push(data.assets.publicPath + cssFile);
-        } else if (local) {
-          data.assets.css.push(path.posix.join(data.assets.publicPath, cssFile));
-        } else {
-          data.assets.css.push(cssUrl(fonts));
+    get api () {
+        if (!this._api) {
+            this._api = new GoogleWebfonts(this.options.apiUrl)
         }
-        if (typeof cb === 'function') {
-          cb(null, data)
-  	}
-			})
-			compilation.plugin("additional-assets", cb => {
-				compilation.chunks.push(this.chunk)
-				compilation.namedChunks[this.options.name] = this.chunk
-				cb()
-			})
-		})
-	}
+        return this._api
+    }
+
+    fetch () {
+        const {
+            fonts,
+            apiUrl,
+            path: fontsPath,
+            filename: cssFile,
+            formats: defaultFormats
+        } = this.options;
+        const compareCss = (a, b) => (a.id.localeCompare(b.id));
+        const fontsCss = [];
+        const files = {};
+        const promises = [];
+
+        fonts.forEach((fontOptions) => {
+            const {
+                family
+            } = fontOptions;
+            const query = this.api.getFontByFamily(family)
+                .then(font => {
+                    if (!font) {
+                        throw new Error(`Font family \"${family}\" not found.`);
+                    }
+                    return font.select(_.assign(
+                        fontOptions, {
+                            formats: defaultFormats
+                        }
+                    ));
+                });
+            const cssRelativePath = path.posix.relative(
+                path.dirname(cssFile),
+                fontsPath
+            );
+            promises.push(
+                query.then(selection => {
+                    return selection.css(cssRelativePath).then((css) => {
+                        fontsCss.push({
+                            css,
+                            id: selection.font.id
+                        })
+                    });
+                })
+            );
+            if (fontsPath) {
+                promises.push(
+                    query.then(q => q.assets())
+                    .then(assets => {
+                        for (const fileName in assets) {
+                            files[fontsPath + fileName] = assets[fileName]
+                        }
+                    })
+                );
+            }
+        });
+        return Promise.all(promises)
+            .then(() => {
+                fontsCss.sort(compareCss);
+                const css = fontsCss.map(font => font.css).join("\n");
+                return {
+                    css,
+                    files
+                };
+            });
+    }
+
+    apply (compiler) {
+        const {
+            fonts,
+            local,
+            filename: cssFile
+        } = this.options
+
+		const htmlWebpackPluginBeforeHtmlGeneration = (data, cb) => {
+            if (local && (data.assets.publicPath.indexOf("://") !== -1 || data.assets.publicPath.indexOf(":") !== -1)) {
+                data.assets.css.push(data.assets.publicPath + cssFile);
+            }
+            else if (local) {
+                data.assets.css.push(path.posix.join(data.assets.publicPath, cssFile));
+            }
+            else {
+                data.assets.css.push(cssUrl(fonts));
+            }
+            if (typeof cb === "function") {
+                cb(null, data)
+            }
+        };
+
+        const make = (compilation, cb) => {
+
+			const additionalAssets = (cb) => {
+	            compilation.chunks.push(this.chunk);
+	            compilation.namedChunks.set(this.options.name, this.chunk);
+	            cb();
+	        };
+
+            if (local) {
+                const addFile = (fileName, source) => {
+                    this.chunk.files.push(fileName)
+                    compilation.assets[fileName] = source
+                }
+                this.fetch().then(({
+                    css,
+                    files
+                }) => {
+                    addFile(cssFile, new RawSource(css));
+                    for (const fileName in files) {
+                        addFile(fileName, files[fileName]);
+                    }
+                    cb();
+                })
+            } else {
+                cb();
+            }
+            if (compilation.hooks) {
+                if (compilation.hooks.htmlWebpackPluginBeforeHtmlGeneration) {
+            	   compilation.hooks.htmlWebpackPluginBeforeHtmlGeneration.tapAsync(pluginSignature, htmlWebpackPluginBeforeHtmlGeneration);
+                }
+            	compilation.hooks.additionalAssets.tapAsync(pluginSignature, additionalAssets);
+            } else {
+	            compilation.plugin("html-webpack-plugin-before-html-generation", htmlWebpackPluginBeforeHtmlGeneration);
+	            compilation.plugin("additional-assets", additionalAssets);
+	        };
+        };
+
+ 		if (compiler.hooks) {
+			compiler.hooks.make.tapAsync(pluginSignature, make);
+		} else {
+        	compiler.plugin("make", make);
+        }
+    }
 }
 
 GoogleWebfontsPlugin.GoogleWebfonts = GoogleWebfonts
